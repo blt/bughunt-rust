50ca04277a353e91fc8cd572c6ee4fda1b38f10b
Brian L. Troutwine <brian@troutwine.us>
2019-03-07 12:23:15 -0800

    attempt auth for gsutil

--- ci/upload_builds.sh
+++ ci/upload_builds.sh
@@ -6,4 +6,5 @@ STR_REPEAT=${BUILD_DIR}/str_repeat
 echo ${TRAVIS_BUILD_NUMBER}
 file ${STR_REPEAT}
 
-gsutil cp ${STR_REPEAT} gs://builds.bughunt.appspot.com/${TRAVIS_BUILD_NUMBER}_str_repeat
+gcloud auth activate-service-account --key-file ci/auth-decrypt.json
+gsutil cp ${STR_REPEAT} gs://builds.bughunt.appspot.com/str_repeat/${TRAVIS_BUILD_NUMBER}_str_repeat
