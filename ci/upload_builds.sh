1de67cf696594d6e6146dc522609e5cbd7fdfd6d
Brian L. Troutwine <brian@troutwine.us>
2019-03-07 15:02:51 -0800

    upload fiddling again

--- ci/upload_builds.sh
+++ ci/upload_builds.sh
@@ -6,5 +6,5 @@ STR_REPEAT=${BUILD_DIR}/str_repeat
 echo ${TRAVIS_BUILD_NUMBER}
 file ${STR_REPEAT}
 
-gcloud auth activate-service-account --key-file ci/auth-decrypt.json
+gcloud auth activate-service-account --key-file ci/auth.json
 gsutil cp ${STR_REPEAT} gs://builds.bughunt.appspot.com/str_repeat/${TRAVIS_BUILD_NUMBER}_str_repeat
