6445818a66be97188c8b5b284db397b602e2cd8b
Brian L. Troutwine <brian@troutwine.us>
2019-03-07 17:17:58 -0800

    improve upload_builds

--- ci/upload_builds.sh
+++ ci/upload_builds.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 BUILD_DIR=target/debug/
-STR_REPEAT=${BUILD_DIR}/str_repeat
-
-echo ${TRAVIS_BUILD_NUMBER}
-file ${STR_REPEAT}
 
 gcloud auth activate-service-account --key-file ci/auth.json
-gsutil cp ${STR_REPEAT} gs://builds.bughunt.appspot.com/str_repeat/${TRAVIS_BUILD_NUMBER}_str_repeat
+for TEST in str_repeat
+do
+    TRGT=${BUILD_DIR}${TEST}.tar.gz
+    tar zcf ${TRGT} ${BUILD_DIR}${TEST}
+    gsutil cp ${TRGT} gs://builds.bughunt.appspot.com/${TEST}/${TRAVIS_BUILD_NUMBER}_${TEST}
+done
