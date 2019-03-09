df90caef46da198b27d46c698967da822ef04204
Brian L. Troutwine <brian@troutwine.us>
2019-03-09 11:52:42 -0800

    debug

--- ci/upload_builds.sh
+++ ci/upload_builds.sh
@@ -5,6 +5,7 @@ source ci/common.sh
 BUILD_DIR=fuzz/target/x86_64-apple-darwin/debug/
 
 gcloud auth activate-service-account --key-file ci/auth.json
+ls ${BUILD_DIR}
 for TEST in ${TESTS}
 do
     TRGT=${BUILD_DIR}${TEST}.tar.gz
